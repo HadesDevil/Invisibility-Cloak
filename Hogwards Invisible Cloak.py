9fa8c78a932e9405a06e94d61b153ff7a7cc20a4
HadesDevil <112417733+HadesDevil@users.noreply.github.com>
2023-03-16 20:05:36 +0530

    Update Hogwards Invisible Cloak.py

--- Hogwards Invisible Cloak.py
+++ Hogwards Invisible Cloak.py
@@ -13,13 +13,18 @@ cv2.resizeWindow("Trackbars",300,300)
 cv2.createTrackbar("L-H","Trackbars",0,179,nothing)
 cv2.createTrackbar("L-S","Trackbars",0,255,nothing)
 cv2.createTrackbar("L-V","Trackbars",0,179,nothing)
-cv2.createTrackbar("U-H","Trackbars",179,179,nothing)
-cv2.createTrackbar("U-S","Trackbars",255,255,nothing)
-cv2.createTrackbar("U-V","Trackbars",255,255,nothing)
+cv2.createTrackbar("U-H","Trackbars",0,179,nothing)
+cv2.createTrackbar("U-S","Trackbars",0,255,nothing)
+cv2.createTrackbar("U-V","Trackbars",0,255,nothing)
 
-
+if not video.isOpened():
+    print("Could not Open video file")
+    
 while True:
     ret,frame=video.read()
+    if not ret:
+        break
+    
     frame=cv2.resize(frame,(520,480))
     image=cv2.resize(image,(520,480))
     hsv=cv2.cvtColor(frame,cv2.COLOR_BGR2HSV)
@@ -37,6 +42,7 @@ while True:
     mask=cv2.inRange(hsv,l_g,u_g)
     res=cv2.bitwise_and(frame,frame,mask=mask)
     f=frame-res
+    
     green_screen=np.where(f==0,image,f)
     cv2.imshow("final",green_screen)
     
